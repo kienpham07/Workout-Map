ac680b723cd8a2b16d5f6af7a65f00cd39cb298b
Kien Pham <kienpham121007.work@gmail.com>
2026-04-16 22:52:38 -0400

    Add MIT license, Leaflet library, and modify some properties for the popup marker whenever user click on the map.

--- script.js
+++ script.js
@@ -16,10 +16,35 @@ if (navigator.geolocation) {
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(latitude, longitude);
       console.log(
         `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu&g_ep=EgoyMDI2MDQxMi4wIKXMDSoASAFQAw%3D%3D`,
       );
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 14);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxHeight: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            }),
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Need to turn on the location');
